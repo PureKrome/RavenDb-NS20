4348568d30c98d1709a8927f87b766ec46b1dd41
Pure Krome <github@world-domination.com.au>
2017-08-27 10:30:16 +1000

    Tried to create a new Embedded Store.

--- tests/RavenDbSample.Tests/UnitTest1.cs
+++ tests/RavenDbSample.Tests/UnitTest1.cs
@@ -1,14 +1,16 @@
-using System;
+using Raven.Client;
+using Raven.Tests.Helpers;
 using Xunit;
 
 namespace RavenDbSample.Tests
 {
-    public class UnitTest1
+    public class UnitTest1 : RavenTestBase
     {
         [Fact]
         public void Test1()
         {
-
+            // Ok - we've not setup the Store/Session for this test. Lets do it...
+            IDocumentStore store = NewDocumentStore(noStaleQueries: true);
         }
     }
 }
