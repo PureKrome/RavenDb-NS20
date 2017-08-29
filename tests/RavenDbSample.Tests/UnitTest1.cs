5fe084f66cd29713695d7e03de0bf46e57d29153
Pure Krome <github@world-domination.com.au>
2017-08-29 23:48:00 +1000

    Using latest NS20 compatible pre-release .

--- tests/RavenDbSample.Tests/UnitTest1.cs
+++ tests/RavenDbSample.Tests/UnitTest1.cs
@@ -9,7 +9,6 @@ namespace RavenDbSample.Tests
         [Fact]
         public void Test1()
         {
-            // Ok - we've not setup the Store/Session for this test. Lets do it...
             IDocumentStore store = NewDocumentStore(noStaleQueries: true);
         }
     }
